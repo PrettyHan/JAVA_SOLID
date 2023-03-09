2d00aee83e294b23b76bc969739e91e7883c4296
hanwoosung <han.woosung@neut.co.jp>
2023-03-09 16:42:57 +0900

    add: 初期設定

---
+++ src/main/java/hello/core/SolidTestApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidTestApplication.class, args);
+	}
+
+}
