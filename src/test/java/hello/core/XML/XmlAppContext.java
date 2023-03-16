185e128a45c7a147678ce2fcfd27fae562282dc4
PrettyHan <imbel9830@gmail.com>
2023-03-16 13:44:03 +0900

    add : appConfigByXML

---
+++ src/test/java/hello/core/XML/XmlAppContext.java
@@ -0,0 +1,22 @@
+package hello.core.XML;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    public void XmlContextTest() {
+
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+
+}
